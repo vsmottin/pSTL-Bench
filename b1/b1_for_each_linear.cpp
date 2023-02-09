6d05801d5bce5daab69402d6bcf3ddbf88b1b3fa
Diego Krupitza <info@diegokrupitza.com>
2023-02-09 11:47:45 +0100

    B1: linear std::for_each skeleton

---
+++ b1/b1_for_each_linear.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <execution>
+
+template<class ExecutionPolicy>
+void for_each_linear(ExecutionPolicy &policy, const std::vector<int> &input_data) {
+
+    std::for_each(policy, input_data.begin(), input_data.end(), [](const auto &entry) {
+        entry = entry + 10;
+    });
+
+}
+
+int main() {
+
+    std::vector<int> x(10, 1);
+
+    for_each_linear(std::execution::par, x);
+
+    return 0;
+}
