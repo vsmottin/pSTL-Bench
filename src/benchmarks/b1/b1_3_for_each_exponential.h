6ec1b68f4b61ae0bc8924fece864710f15657311
Diego Krupitza <info@diegokrupitza.com>
2023-02-13 09:47:41 +0100

    B1: exeponential benchmark

---
+++ src/benchmarks/b1/b1_3_for_each_exponential.h
@@ -0,0 +1,35 @@
+#ifndef MASTER_BENCHMARKS_B1_3_FOR_EACH_EXPONENTIAL_H
+#define MASTER_BENCHMARKS_B1_3_FOR_EACH_EXPONENTIAL_H
+
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <ranges>
+#include <iostream>
+
+//TODO: does it make sense to have such a benchmark??
+template<class ExecutionPolicy>
+struct FIB {
+
+    ExecutionPolicy m_policy;
+
+    explicit FIB(ExecutionPolicy &policy) : m_policy(policy) {}
+
+    void operator()(const int &index) {
+        if (index <= 0) {
+            return;
+        }
+
+        std::vector<int> next{index - 1, index - 2};
+        std::for_each(m_policy, next.begin(), next.end(), FIB{m_policy});
+    }
+};
+
+template<class ExecutionPolicy, typename T>
+inline void b1_3_for_each_exponential(ExecutionPolicy &policy, const T &input_data) {
+
+    std::for_each(policy, input_data.begin(), input_data.end(), FIB{policy});
+
+}
+
+#endif //MASTER_BENCHMARKS_B1_3_FOR_EACH_EXPONENTIAL_H
--- src/benchmarks/b1/b1_group.h
+++ src/benchmarks/b1/b1_group.h
@@ -2,9 +2,11 @@
 #define MASTER_BENCHMARKS_B1_GROUP_H
 
 #include <execution>
+#include <ranges>
 #include <benchmark/benchmark.h>
 #include "b1_1_for_each_linear.h"
 #include "b1_2_for_each_quadratic.h"
+#include "b1_3_for_each_exponential.h"
 
 //region b1_1_for_each_linear
 
@@ -69,9 +71,36 @@ static void b1_2_for_each_quadratic_par_unseq(benchmark::State &state) {
 
 //endregion b1_2_for_each_quadratic
 
+//region b1_3_for_each_exponential
+
+
+static void b1_3_for_each_exponential_seq(benchmark::State &state) {
+    const auto &data = std::ranges::iota_view(1, static_cast<int>(state.range(0)));
+    for (auto _: state) {
+        b1_3_for_each_exponential(std::execution::seq, data);
+    }
+}
+
+static void b1_3_for_each_exponential_par(benchmark::State &state) {
+    const auto &data = std::ranges::iota_view(1, static_cast<int>(state.range(0)));
+    for (auto _: state) {
+        b1_3_for_each_exponential(std::execution::par, data);
+    }
+}
+
+static void b1_3_for_each_exponential_par_unseq(benchmark::State &state) {
+    const auto &data = std::ranges::iota_view(1, static_cast<int>(state.range(0)));
+    for (auto _: state) {
+        b1_3_for_each_exponential(std::execution::par_unseq, data);
+    }
+}
+
+
+//endregion b1_3_for_each_exponential
 
 // Register the function as a benchmark
 #define B1_GROUP_BENCHMARKS \
+                            \
                             \
     BENCHMARK(b1_1_for_each_linear_seq)->RangeMultiplier(2)->Range(1 << 5, 1 << 20); \
     BENCHMARK(b1_1_for_each_linear_par)->RangeMultiplier(2)->Range(1 << 5, 1 << 20); \
@@ -81,5 +110,11 @@ static void b1_2_for_each_quadratic_par_unseq(benchmark::State &state) {
     BENCHMARK(b1_2_for_each_quadratic_seq)->RangeMultiplier(2)->Range(1 << 5, 1 << 15); \
     BENCHMARK(b1_2_for_each_quadratic_par)->RangeMultiplier(2)->Range(1 << 5, 1 << 15); \
     BENCHMARK(b1_2_for_each_quadratic_par_unseq)->RangeMultiplier(2)->Range(1 << 5, 1 << 15); \
+                            \
+                            \
+    BENCHMARK(b1_3_for_each_exponential_seq)->RangeMultiplier(2)->Range(1 << 2, 1 << 5); \
+    BENCHMARK(b1_3_for_each_exponential_par)->RangeMultiplier(2)->Range(1 << 2, 1 << 5); \
+    BENCHMARK(b1_3_for_each_exponential_par_unseq)->RangeMultiplier(2)->Range(1 << 2, 1 << 5); \
+
 
 #endif //MASTER_BENCHMARKS_B1_GROUP_H
