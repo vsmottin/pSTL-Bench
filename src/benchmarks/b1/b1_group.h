7c738f22786e782a7e72b5df54bd99196d88788f
Diego Krupitza <info@diegokrupitza.com>
2023-02-13 15:46:25 +0100

    B1: cleanedup the benchmark by using templates and constexpr

--- src/benchmarks/b1/b1_group.h
+++ src/benchmarks/b1/b1_group.h
@@ -11,30 +11,14 @@
 
 //region b1_1_for_each_linear
 
-static void b1_1_for_each_linear_seq(benchmark::State &state) {
+template<class Policy>
+static void b1_1_for_each_linear_wrapper(benchmark::State &state) {
+    constexpr auto execution_policy = Policy{};
 
     std::vector<int> x(state.range(0), 1);
 
     for (auto _: state) {
-        b1_1_for_each_linear(std::execution::seq, x);
-    }
-}
-
-static void b1_1_for_each_linear_par(benchmark::State &state) {
-
-    std::vector<int> x(state.range(0), 1);
-
-    for (auto _: state) {
-        b1_1_for_each_linear(std::execution::par, x);
-    }
-}
-
-static void b1_1_for_each_linear_par_unseq(benchmark::State &state) {
-
-    std::vector<int> x(state.range(0), 1);
-
-    for (auto _: state) {
-        b1_1_for_each_linear(std::execution::par_unseq, x);
+        b1_1_for_each_linear(execution_policy, x);
     }
 }
 
@@ -43,30 +27,14 @@ static void b1_1_for_each_linear_par_unseq(benchmark::State &state) {
 
 //region b1_2_for_each_quadratic
 
-static void b1_2_for_each_quadratic_seq(benchmark::State &state) {
+template<class Policy>
+static void b1_2_for_each_quadratic_wrapper(benchmark::State &state) {
+    constexpr auto execution_policy = Policy{};
 
     std::vector<int> x(state.range(0), 1);
 
     for (auto _: state) {
-        b1_2_for_each_quadratic(std::execution::seq, x);
-    }
-}
-
-static void b1_2_for_each_quadratic_par(benchmark::State &state) {
-
-    std::vector<int> x(state.range(0), 1);
-
-    for (auto _: state) {
-        b1_2_for_each_quadratic(std::execution::par, x);
-    }
-}
-
-static void b1_2_for_each_quadratic_par_unseq(benchmark::State &state) {
-
-    std::vector<int> x(state.range(0), 1);
-
-    for (auto _: state) {
-        b1_2_for_each_quadratic(std::execution::par_unseq, x);
+        b1_2_for_each_quadratic(execution_policy, x);
     }
 }
 
@@ -74,28 +42,16 @@ static void b1_2_for_each_quadratic_par_unseq(benchmark::State &state) {
 
 //region b1_3_for_each_exponential
 
+template<class Policy>
+static void b1_3_for_each_exponential_wrapper(benchmark::State &state) {
+    constexpr auto execution_policy = Policy{};
 
-static void b1_3_for_each_exponential_seq(benchmark::State &state) {
     const auto &data = std::ranges::iota_view(1, static_cast<int>(state.range(0)));
+
     for (auto _: state) {
-        b1_3_for_each_exponential(std::execution::seq, data);
+        b1_3_for_each_exponential(execution_policy, data);
     }
 }
-
-static void b1_3_for_each_exponential_par(benchmark::State &state) {
-    const auto &data = std::ranges::iota_view(1, static_cast<int>(state.range(0)));
-    for (auto _: state) {
-        b1_3_for_each_exponential(std::execution::par, data);
-    }
-}
-
-static void b1_3_for_each_exponential_par_unseq(benchmark::State &state) {
-    const auto &data = std::ranges::iota_view(1, static_cast<int>(state.range(0)));
-    for (auto _: state) {
-        b1_3_for_each_exponential(std::execution::par_unseq, data);
-    }
-}
-
 
 //endregion b1_3_for_each_exponential
 
@@ -103,18 +59,18 @@ static void b1_3_for_each_exponential_par_unseq(benchmark::State &state) {
 #define B1_GROUP_BENCHMARKS \
                             \
                             \
-    BENCHMARK(b1_1_for_each_linear_seq)->Name(BENCHMARK_NAME("b1_1_for_each_linear_seq"))->RangeMultiplier(2)->Range(1 << 5, 1 << 20); \
-    BENCHMARK(b1_1_for_each_linear_par)->Name(BENCHMARK_NAME("b1_1_for_each_linear_par"))->RangeMultiplier(2)->Range(1 << 5, 1 << 20); \
-    BENCHMARK(b1_1_for_each_linear_par_unseq)->Name(BENCHMARK_NAME("b1_1_for_each_linear_par_unseq"))->RangeMultiplier(2)->Range(1 << 5, 1 << 20); \
+    BENCHMARK_TEMPLATE1(b1_1_for_each_linear_wrapper,std::execution::sequenced_policy)->Name(BENCHMARK_NAME("b1_1_for_each_linear_seq"))->RangeMultiplier(2)->Range(1 << 5, 1 << 20); \
+    BENCHMARK_TEMPLATE1(b1_1_for_each_linear_wrapper,std::execution::parallel_policy)->Name(BENCHMARK_NAME("b1_1_for_each_linear_par"))->RangeMultiplier(2)->Range(1 << 5, 1 << 20); \
+    BENCHMARK_TEMPLATE1(b1_1_for_each_linear_wrapper,std::execution::parallel_unsequenced_policy)->Name(BENCHMARK_NAME("b1_1_for_each_linear_par_unseq"))->RangeMultiplier(2)->Range(1 << 5, 1 << 20); \
                              \
                              \
-    BENCHMARK(b1_2_for_each_quadratic_seq)->Name(BENCHMARK_NAME("b1_2_for_each_quadratic_seq"))->RangeMultiplier(2)->Range(1 << 5, 1 << 15); \
-    BENCHMARK(b1_2_for_each_quadratic_par)->Name(BENCHMARK_NAME("b1_2_for_each_quadratic_par"))->RangeMultiplier(2)->Range(1 << 5, 1 << 15); \
-    BENCHMARK(b1_2_for_each_quadratic_par_unseq)->Name(BENCHMARK_NAME("b1_2_for_each_quadratic_par_unseq"))->RangeMultiplier(2)->Range(1 << 5, 1 << 15); \
+    BENCHMARK_TEMPLATE1(b1_2_for_each_quadratic_wrapper,std::execution::sequenced_policy)->Name(BENCHMARK_NAME("b1_2_for_each_quadratic_seq"))->RangeMultiplier(2)->Range(1 << 5, 1 << 15); \
+    BENCHMARK_TEMPLATE1(b1_2_for_each_quadratic_wrapper,std::execution::parallel_policy)->Name(BENCHMARK_NAME("b1_2_for_each_quadratic_par"))->RangeMultiplier(2)->Range(1 << 5, 1 << 15); \
+    BENCHMARK_TEMPLATE1(b1_2_for_each_quadratic_wrapper,std::execution::parallel_unsequenced_policy)->Name(BENCHMARK_NAME("b1_2_for_each_quadratic_par_unseq"))->RangeMultiplier(2)->Range(1 << 5, 1 << 15); \
                             \
                             \
-    BENCHMARK(b1_3_for_each_exponential_seq)->Name(BENCHMARK_NAME("b1_3_for_each_exponential_seq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 5); \
-    BENCHMARK(b1_3_for_each_exponential_par)->Name(BENCHMARK_NAME("b1_3_for_each_exponential_par"))->RangeMultiplier(2)->Range(1 << 2, 1 << 5); \
-    BENCHMARK(b1_3_for_each_exponential_par_unseq)->Name(BENCHMARK_NAME("b1_3_for_each_exponential_par_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 5);
+    BENCHMARK_TEMPLATE1(b1_3_for_each_exponential_wrapper,std::execution::sequenced_policy)->Name(BENCHMARK_NAME("b1_3_for_each_exponential_seq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 5); \
+    BENCHMARK_TEMPLATE1(b1_3_for_each_exponential_wrapper,std::execution::parallel_policy)->Name(BENCHMARK_NAME("b1_3_for_each_exponential_par"))->RangeMultiplier(2)->Range(1 << 2, 1 << 5); \
+    BENCHMARK_TEMPLATE1(b1_3_for_each_exponential_wrapper,std::execution::parallel_unsequenced_policy)->Name(BENCHMARK_NAME("b1_3_for_each_exponential_par_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 5);
 
 #endif //MASTER_BENCHMARKS_B1_GROUP_H
