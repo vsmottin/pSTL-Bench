9f239f5a9a30e0ecd4021fccc664f845b5c48c23
Diego Krupitza <info@diegokrupitza.com>
2023-02-16 17:49:31 +0100

    B4: Added to benchmark main

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include "benchmarks/b1/b1_group.h"
 #include "benchmarks/b2/b2_group.h"
 #include "benchmarks/b3/b3_group.h"
+#include "benchmarks/b4/b4_group.h"
 
 B1_GROUP_BENCHMARKS
 B2_GROUP_BENCHMARKS
 B3_GROUP_BENCHMARKS
+B4_GROUP_BENCHMARKS
 
 // Run the benchmark
 BENCHMARK_MAIN();
