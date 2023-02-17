54dbf6635a1d6d4174322aa557f08cd1a081636b
Diego Krupitza <info@diegokrupitza.com>
2023-02-17 11:31:21 +0100

    B4: added stable sort cutoff benchmark

---
+++ src/benchmarks/b4/b4_2_stable_sort_cutoff.h
@@ -0,0 +1,16 @@
+
+#ifndef MASTER_BENCHMARKS_B4_2_STABLE_SORT_CUTOFF_H
+#define MASTER_BENCHMARKS_B4_2_STABLE_SORT_CUTOFF_H
+
+#include <algorithm>
+#include <vector>
+
+template<class ExecutionPolicy>
+inline void
+b4_2_stable_sort_cutoff(ExecutionPolicy &&policy, std::vector<int> &input_data) {
+
+    std::stable_sort(policy, input_data.begin(), input_data.end());
+
+}
+
+#endif //MASTER_BENCHMARKS_B4_2_STABLE_SORT_CUTOFF_H
--- src/benchmarks/b4/b4_group.h
+++ src/benchmarks/b4/b4_group.h
@@ -11,6 +11,7 @@
 #include "../benchmark_prefix.h"
 #include "../benchmark_utils.h"
 #include "b4_1_merge_cutoff.h"
+#include "b4_2_stable_sort_cutoff.h"
 
 //region b4_1_merge_cutoff
 
@@ -38,12 +39,87 @@ static void b4_1_merge_cutoff_wrapper(benchmark::State &state) {
 
 //endregion b4_1_merge_cutoff
 
+
+//region b4_2_stable_sort_cutoff
+
+template<class Policy>
+static void b4_2_stable_sort_cutoff_already_sorted_wrapper(benchmark::State &state) {
+    constexpr auto execution_policy = Policy{};
+
+    const auto &size = state.range(0);
+
+    auto already_sorted_vec = suite::generate_increment_vec<int>(size, 1);
+
+    for (auto _: state) {
+        b4_2_stable_sort_cutoff(execution_policy, already_sorted_vec);
+
+        state.PauseTiming();
+        assert(already_sorted_vec[0] <= already_sorted_vec[1]);
+        state.ResumeTiming();
+    }
+}
+
+template<class Policy>
+static void b4_2_stable_sort_cutoff_not_sorted_wrapper(benchmark::State &state) {
+    constexpr auto execution_policy = Policy{};
+
+    const auto &size = state.range(0);
+
+    auto already_sorted_vec = suite::generate_uniform_dist_vec<int>(size, 50, 100);
+
+    for (auto _: state) {
+        b4_2_stable_sort_cutoff(execution_policy, already_sorted_vec);
+
+        state.PauseTiming();
+        assert(already_sorted_vec[0] <= already_sorted_vec[1]);
+        state.ResumeTiming();
+    }
+}
+
+template<class Policy>
+static void b4_2_stable_sort_cutoff_decrement_sorted_wrapper(benchmark::State &state) {
+    constexpr auto execution_policy = Policy{};
+
+    const auto &size = state.range(0);
+
+    auto already_sorted_vec = suite::generate_decrement_vec<int>(size, size + 1, 1);
+
+    for (auto _: state) {
+        b4_2_stable_sort_cutoff(execution_policy, already_sorted_vec);
+
+        state.PauseTiming();
+        assert(already_sorted_vec[0] <= already_sorted_vec[1]);
+        state.ResumeTiming();
+    }
+}
+
+//endregion b4_2_stable_sort_cutoff
+
 // Register the function as a benchmark
 #define B4_GROUP_BENCHMARKS \
+                            \
         BENCHMARK_TEMPLATE1(b4_1_merge_cutoff_wrapper,std::execution::sequenced_policy)->Name(BENCHMARK_NAME("b4_1_merge_cutoff_wrapper_seq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
         BENCHMARK_TEMPLATE1(b4_1_merge_cutoff_wrapper,std::execution::parallel_policy)->Name(BENCHMARK_NAME("b4_1_merge_cutoff_wrapper_par"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20);     \
         BENCHMARK_TEMPLATE1(b4_1_merge_cutoff_wrapper,std::execution::parallel_unsequenced_policy)->Name(BENCHMARK_NAME("b4_1_merge_cutoff_wrapper_par_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
         BENCHMARK_TEMPLATE1(b4_1_merge_cutoff_wrapper,std::execution::unsequenced_policy)->Name(BENCHMARK_NAME("b4_1_merge_cutoff_wrapper_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
+                            \
+                            \
+        BENCHMARK_TEMPLATE1(b4_2_stable_sort_cutoff_already_sorted_wrapper,std::execution::sequenced_policy)->Name(BENCHMARK_NAME("b4_2_stable_sort_cutoff_already_sorted_seq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
+        BENCHMARK_TEMPLATE1(b4_2_stable_sort_cutoff_already_sorted_wrapper,std::execution::parallel_policy)->Name(BENCHMARK_NAME("b4_2_stable_sort_cutoff_already_sorted_par"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20);     \
+        BENCHMARK_TEMPLATE1(b4_2_stable_sort_cutoff_already_sorted_wrapper,std::execution::parallel_unsequenced_policy)->Name(BENCHMARK_NAME("b4_2_stable_sort_cutoff_already_sorted_par_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
+        BENCHMARK_TEMPLATE1(b4_2_stable_sort_cutoff_already_sorted_wrapper,std::execution::unsequenced_policy)->Name(BENCHMARK_NAME("b4_2_stable_sort_cutoff_already_sorted_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20);\
+                            \
+                            \
+        BENCHMARK_TEMPLATE1(b4_2_stable_sort_cutoff_decrement_sorted_wrapper,std::execution::sequenced_policy)->Name(BENCHMARK_NAME("b4_2_stable_sort_cutoff_decrement_sorted_seq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
+        BENCHMARK_TEMPLATE1(b4_2_stable_sort_cutoff_decrement_sorted_wrapper,std::execution::parallel_policy)->Name(BENCHMARK_NAME("b4_2_stable_sort_cutoff_decrement_sorted_par"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20);     \
+        BENCHMARK_TEMPLATE1(b4_2_stable_sort_cutoff_decrement_sorted_wrapper,std::execution::parallel_unsequenced_policy)->Name(BENCHMARK_NAME("b4_2_stable_sort_cutoff_decrement_sorted_par_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
+        BENCHMARK_TEMPLATE1(b4_2_stable_sort_cutoff_decrement_sorted_wrapper,std::execution::unsequenced_policy)->Name(BENCHMARK_NAME("b4_2_stable_sort_cutoff_decrement_sorted_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20);     \
+                            \
+                            \
+        BENCHMARK_TEMPLATE1(b4_2_stable_sort_cutoff_not_sorted_wrapper,std::execution::sequenced_policy)->Name(BENCHMARK_NAME("b4_2_stable_sort_cutoff_not_sorted_seq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
+        BENCHMARK_TEMPLATE1(b4_2_stable_sort_cutoff_not_sorted_wrapper,std::execution::parallel_policy)->Name(BENCHMARK_NAME("b4_2_stable_sort_cutoff_not_sorted_par"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20);     \
+        BENCHMARK_TEMPLATE1(b4_2_stable_sort_cutoff_not_sorted_wrapper,std::execution::parallel_unsequenced_policy)->Name(BENCHMARK_NAME("b4_2_stable_sort_cutoff_not_sorted_par_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
+        BENCHMARK_TEMPLATE1(b4_2_stable_sort_cutoff_not_sorted_wrapper,std::execution::unsequenced_policy)->Name(BENCHMARK_NAME("b4_2_stable_sort_cutoff_not_sorted_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20);     \
 
 
 #endif //MASTER_BENCHMARKS_B4_GROUP_H
