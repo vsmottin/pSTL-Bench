842ac26f0bee63455407c0b5e6354cff748d5447
Diego Krupitza <info@diegokrupitza.com>
2023-02-20 16:13:09 +0100

    B5: added skeleton for group b5

---
+++ src/benchmarks/b5/b5_group.h
@@ -0,0 +1,15 @@
+
+#ifndef MASTER_BENCHMARKS_B5_GROUP_H
+#define MASTER_BENCHMARKS_B5_GROUP_H
+
+#include <benchmark/benchmark.h>
+#include <execution>
+#include <vector>
+#include <cassert>
+#include <iostream>
+
+// Register the function as a benchmark
+#define B5_GROUP_BENCHMARKS \
+
+
+#endif //MASTER_BENCHMARKS_B5_GROUP_H
