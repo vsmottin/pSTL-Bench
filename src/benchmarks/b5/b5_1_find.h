4bd236ef3ffcbcb880c018e08b55076f27946ca2
Diego Krupitza <info@diegokrupitza.com>
2023-02-21 09:37:27 +0100

    B5: std::find for different container types

---
+++ src/benchmarks/b5/b5_1_find.h
@@ -0,0 +1,15 @@
+
+#ifndef MASTER_BENCHMARKS_B5_1_FIND_H
+#define MASTER_BENCHMARKS_B5_1_FIND_H
+
+#include <algorithm>
+#include <vector>
+
+template<class ExecutionPolicy, class C,
+        typename T = typename C::value_type,
+        typename Iterator = typename C::const_iterator>
+inline Iterator b5_1_find(ExecutionPolicy &&policy, const C &container, const T &find_val) {
+    return std::find(policy, container.begin(), container.end(), find_val);
+}
+
+#endif //MASTER_BENCHMARKS_B5_1_FIND_H
--- src/benchmarks/b5/b5_group.h
+++ src/benchmarks/b5/b5_group.h
@@ -7,9 +7,99 @@
 #include <vector>
 #include <cassert>
 #include <iostream>
+#include <list>
+#include <deque>
+
+#include "../benchmark_prefix.h"
+#include "../benchmark_utils.h"
+
+#include "b5_1_find.h"
+
+template<class Policy, class Container>
+static void b5_1_find_first_entry(benchmark::State &state) {
+    constexpr auto execution_policy = Policy{};
+
+    const auto &size = state.range(0);
+
+    // vector with values [0,size)
+    const Container vec1 = suite::generate_increment<Container>(size, 1);
+
+    for (auto _: state) {
+        auto find_location = b5_1_find(execution_policy, vec1, 0);
+
+        state.PauseTiming();
+        // make sure the val is really found
+        assert(find_location != vec1.end());
+        state.ResumeTiming();
+    }
+}
+
+template<class Policy, class Container>
+static void b5_1_find_last_entry(benchmark::State &state) {
+    constexpr auto execution_policy = Policy{};
+
+    const auto &size = state.range(0);
+
+    // vector with values [0,size)
+    const auto vec1 = suite::generate_increment<Container>(size, 1);
+
+    for (auto _: state) {
+        auto find_location = b5_1_find(execution_policy, vec1, size - 1);
+
+        state.PauseTiming();
+        // make sure the val is really found
+        assert(find_location != vec1.end());
+        state.ResumeTiming();
+    }
+}
+
+template<class Policy, class Container>
+static void b5_1_find_non_existing_entry(benchmark::State &state) {
+    constexpr auto execution_policy = Policy{};
+
+    const auto &size = state.range(0);
+
+    // vector with values [0,size)
+    const auto vec1 = suite::generate_increment<Container>(size, 1);
+
+    for (auto _: state) {
+        auto find_location = b5_1_find(execution_policy, vec1, -10);
+
+        state.PauseTiming();
+        // make sure the val is really found
+        assert(find_location == vec1.end());
+        state.ResumeTiming();
+    }
+}
+
+#define B5_1_FIND_WRAPPER(datatype) \
+                                    \
+        BENCHMARK_TEMPLATE2(b5_1_find_first_entry,std::execution::sequenced_policy, datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_first_entry_seq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
+        BENCHMARK_TEMPLATE2(b5_1_find_first_entry,std::execution::parallel_policy, datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_first_entry_par"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20);     \
+        BENCHMARK_TEMPLATE2(b5_1_find_first_entry,std::execution::parallel_unsequenced_policy, datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_first_entry_par_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
+        BENCHMARK_TEMPLATE2(b5_1_find_first_entry,std::execution::unsequenced_policy, datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_first_entry_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20);\
+                            \
+                            \
+        BENCHMARK_TEMPLATE2(b5_1_find_last_entry,std::execution::sequenced_policy,datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_last_entry_seq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
+        BENCHMARK_TEMPLATE2(b5_1_find_last_entry,std::execution::parallel_policy,datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_last_entry_par"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20);     \
+        BENCHMARK_TEMPLATE2(b5_1_find_last_entry,std::execution::parallel_unsequenced_policy,datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_last_entry_par_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
+        BENCHMARK_TEMPLATE2(b5_1_find_last_entry,std::execution::unsequenced_policy,datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_last_entry_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20);\
+                            \
+                            \
+        BENCHMARK_TEMPLATE2(b5_1_find_non_existing_entry,std::execution::sequenced_policy,datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_non_existing_entry_seq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
+        BENCHMARK_TEMPLATE2(b5_1_find_non_existing_entry,std::execution::parallel_policy,datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_non_existing_entry_par"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20);     \
+        BENCHMARK_TEMPLATE2(b5_1_find_non_existing_entry,std::execution::parallel_unsequenced_policy,datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_non_existing_entry_par_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
+        BENCHMARK_TEMPLATE2(b5_1_find_non_existing_entry,std::execution::unsequenced_policy,datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_non_existing_entry_unseq"))->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
+
+
+
 
 // Register the function as a benchmark
 #define B5_GROUP_BENCHMARKS \
+                            \
+        B5_1_FIND_WRAPPER(std::vector<int>) \
+        B5_1_FIND_WRAPPER(std::deque<int>)  \
+        B5_1_FIND_WRAPPER(std::list<int>)
 
 
 #endif //MASTER_BENCHMARKS_B5_GROUP_H
