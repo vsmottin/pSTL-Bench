4ea2b7f949118dc1845c93580953b52dc99531ee
Diego Krupitza <info@diegokrupitza.com>
2023-02-22 10:10:04 +0100

    B6: added skeleton

---
+++ src/benchmarks/b6/b6_group.h
@@ -0,0 +1,15 @@
+
+#ifndef MASTER_BENCHMARKS_B6_GROUP_H
+#define MASTER_BENCHMARKS_B6_GROUP_H
+
+#include <benchmark/benchmark.h>
+
+#include "../benchmark_prefix.h"
+#include "../benchmark_utils.h"
+
+
+#define B6_GROUP_BENCHMARKS \
+
+
+
+#endif //MASTER_BENCHMARKS_B6_GROUP_H
--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,14 @@
 #include "benchmarks/b3/b3_group.h"
 #include "benchmarks/b4/b4_group.h"
 #include "benchmarks/b5/b5_group.h"
+#include "benchmarks/b6/b6_group.h"
 
 B1_GROUP_BENCHMARKS
 B2_GROUP_BENCHMARKS
 B3_GROUP_BENCHMARKS
 B4_GROUP_BENCHMARKS
 B5_GROUP_BENCHMARKS
+B6_GROUP_BENCHMARKS
 
 // Run the benchmark
 BENCHMARK_MAIN();
