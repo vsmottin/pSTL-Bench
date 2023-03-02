be6254bfe19a06cca34b1d11661aed80cbf48ed0
Diego Krupitza <info@diegokrupitza.com>
2023-03-02 16:43:13 +0100

    Added Benchmarkgenerator script to populate configuration

---
+++ benchmarkconfig_generator/generator.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+input_filename = 'list_of_benchmarks.txt'
+
+with open(input_filename) as file:
+    lines = [line.rstrip() for line in file if not line.startswith('#') and len(line.strip()) > 0]
+
+output = []
+for line in lines:
+    output.append({
+        "name": f"{line}_Default",
+        "description": f"Running the {line} benchmarks without any constraints",
+        "type": "DEFAULT",
+        "regex_filter": line
+    })
+
+print(json.dumps(output))
