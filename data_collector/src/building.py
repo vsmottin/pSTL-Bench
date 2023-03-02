6375c9948f0f6e3e2f222839fb6579ead47bebb0
Diego Krupitza <info@diegokrupitza.com>
2023-03-02 12:21:45 +0100

    DataCollector: remove mistake comment to use cxxflags

--- data_collector/src/building.py
+++ data_collector/src/building.py
@@ -41,8 +41,12 @@ def build_binary(compiler: Compiler, configuration: Config) -> None:
     :return:
     """
     logger.info("Starting binary build")
+    build_artifact_folder = get_build_artifact_folder_for_compiler(compiler, configuration)
+
+    logger.info(f"Binary build location: {build_artifact_folder}")
+
     binary_build_args = ['cmake',
-                         '--build', get_build_artifact_folder_for_compiler(compiler, configuration),
+                         '--build', build_artifact_folder,
                          '--target', configuration.binary_target,
                          '-j', '8'
                          ]
@@ -66,10 +70,14 @@ def generate_ninja_cmake(compiler: Compiler, configuration: Config) -> None:
                               '-G', 'Ninja',
                               '-DCMAKE_BUILD_TYPE=Release',
                               f'-DBENCHMARK_PREFIX={compiler.name}',
-                              # f'-DCMAKE_CXX_FLAGS="{compiler.CXX_FLAGS}"',
+                              f'-DCMAKE_CXX_FLAGS="{compiler.CXX_FLAGS}"',
                               '-S', configuration.cmake_location,
                               '-B', get_build_artifact_folder_for_compiler(compiler, configuration)
                               ]
+
+    logger.debug(f"Using the cmake ninja command: {(' '.join(cmake_ninja_build_args))}")
+
     start_subprocess_and_log(cmake_ninja_build_args, prefix=f"{compiler.name}-cmake-ninja",
                              error_msg=f"Could not build: {compiler.name}")
+    
     logger.info("Successfully completed cmake ninja build")
