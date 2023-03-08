92f84ca2b1e56b30c0553d9652dba7fd3785148c
Diego Krupitza <info@diegokrupitza.com>
2023-03-08 14:56:23 +0100

    B9: Removed useless testcase

--- src/benchmarks/b9/b9_group.h
+++ src/benchmarks/b9/b9_group.h
@@ -13,7 +13,6 @@
 #include "b9_1_transform_baseline.h"
 #include "b9_2_transform_old_iota.h"
 #include "b9_3_transform_views_iota.h"
-#include "b9_4_transform_views_iota_common.h"
 #include "b9_5_transform_custom_iterator.h"
 #include "b9_6_transform_boost.h"
 
@@ -116,39 +115,6 @@ static void b9_3_transform_views_iota_wrapper(benchmark::State &state) {
 
 //endregion b9_3_transform_views_iota
 
-//region b9_4_transform_views_iota_common
-
-template<class Policy>
-static void b9_4_transform_views_iota_common_wrapper(benchmark::State &state) {
-    constexpr auto execution_policy = Policy{};
-
-    const auto &size = state.range(0);
-    const auto last_element = size - 1;
-
-    const auto input_data = suite::generate_uniform_dist_vec<int>(size, 0, 10);
-
-    std::vector<int> res(size);
-
-    for (auto _: state) {
-        B9::b9_4_transform_views_iota_common(execution_policy, input_data, res);
-
-        state.PauseTiming();
-        assert((res[0] == input_data[0] + 10));
-        assert((res[last_element] == input_data[last_element] + 10));
-        state.ResumeTiming();
-    }
-}
-
-#define B9_4_TRANSFORM_VIEWS_IOTA_COMMON_WRAPPER \
-                                        \
-        BENCHMARK_TEMPLATE1(b9_4_transform_views_iota_common_wrapper,std::execution::sequenced_policy)->Name(BENCHMARK_NAME("b9_4_transform_views_iota_common_seq"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
-        BENCHMARK_TEMPLATE1(b9_4_transform_views_iota_common_wrapper,std::execution::parallel_policy)->Name(BENCHMARK_NAME("b9_4_transform_views_iota_common_par"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(1 << 2, 1 << 20);     \
-        BENCHMARK_TEMPLATE1(b9_4_transform_views_iota_common_wrapper,std::execution::parallel_unsequenced_policy)->Name(BENCHMARK_NAME("b9_4_transform_views_iota_common_par_unseq"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(1 << 2, 1 << 20); \
-        BENCHMARK_TEMPLATE1(b9_4_transform_views_iota_common_wrapper,std::execution::unsequenced_policy)->Name(BENCHMARK_NAME("b9_4_transform_views_iota_common_unseq"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(1 << 2, 1 << 20);\
-
-
-//endregion b9_4_transform_views_iota_common
-
 //region b9_5_transform_custom_iterator
 
 template<class Policy>
@@ -225,9 +191,6 @@ static void b9_6_transform_boost_wrapper(benchmark::State &state) {
                             \
                             \
             B9_3_TRANSFORM_VIEWS_IOTA_WRAPPER \
-                            \
-                            \
-            B9_4_TRANSFORM_VIEWS_IOTA_COMMON_WRAPPER \
                             \
                             \
             B9_5_TRANSFORM_CUSTOM_ITERATOR_WRAPPER \
