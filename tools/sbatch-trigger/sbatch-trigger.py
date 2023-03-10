6f78c5ea5792cc1abfe235d0d80173128cd3b2d9
Diego Krupitza <info@diegokrupitza.com>
2023-03-10 11:11:16 +0100

    SbatchTrigger: added base to trigger jobs with sbatch from a dir

---
+++ tools/sbatch-trigger/sbatch-trigger.py
@@ -0,0 +1,52 @@
+import os
+from pathlib import Path
+import getopt
+import sys
+import subprocess
+
+
+def get_params_from_argv(argv) -> Tuple[str, bool]:
+    target_dir = ""
+    only_count = False
+
+    opts, args = getopt.getopt(argv, "hct:", ["target-dir="])
+    for opt, arg in opts:
+        if opt == '-h':
+            print('main.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ["-t", "--target-dir"]:
+            target_dir = arg
+        elif opt in ["-c"]:
+            only_count = True
+        else:
+            print("Wrong parameter! Could not find:", opt)
+            sys.exit()
+    return target_dir, only_count
+
+
+if __name__ == "__main__":
+    target_dir, only_count = get_params_from_argv(sys.argv[1:])
+
+    files_to_trigger = []
+
+    for dirpath, dirnames, filenames in os.walk(target_dir):
+        if not dirnames:
+            if only_count:
+                print(f"{dirpath} has {(len(filenames))} files")
+
+            files_to_trigger.extend([os.path.join(dirpath, x) for x in filenames])
+
+    print(f"Total number of batch files: {len(files_to_trigger)} ")
+
+    if only_count:
+        exit(0)
+
+    for filename in files_to_trigger:
+        args = ["sbatch", filename]
+        process = subprocess.Popen(args, stdout=subprocess.STDOUT, stderr=subprocess.STDOUT)
+
+        exitcode = process.wait()  # 0 means success
+
+        if exitcode != 0:
+            print(f"Error triggering: {(' '.join(args))}")
+            exit(1)
