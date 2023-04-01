2ef7f918648bec558f057ec6a634339f3e366cfd
Diego Krupitza <info@diegokrupitza.com>
2023-04-01 16:03:16 +0200

    B5: used new allocator options

--- src/benchmarks/b5/b5_4_minmax_element.h
+++ src/benchmarks/b5/b5_4_minmax_element.h
@@ -8,9 +8,13 @@
 
 namespace B5 {
 
-    template<class ExecutionPolicy>
-    inline std::pair<std::vector<int>::const_iterator, std::vector<int>::const_iterator>
-    b5_4_minmax_element(ExecutionPolicy &&policy, const std::vector<int> &container) {
+    template<class ExecutionPolicy,
+            typename BASE_POLICY = typename suite::base_type<ExecutionPolicy>,
+            typename VEC = typename suite::int_vec<BASE_POLICY>,
+            typename CONST_ITERATOR = VEC::const_iterator
+    >
+    inline std::pair<CONST_ITERATOR, CONST_ITERATOR>
+    b5_4_minmax_element(ExecutionPolicy &&policy, const VEC &container) {
         return std::minmax_element(policy, container.begin(), container.end());
     }
 
--- src/benchmarks/b5/b5_group.h
+++ src/benchmarks/b5/b5_group.h
@@ -21,14 +21,14 @@
 
 //region b5_1_find
 
-template<class Policy, class Container>
+template<class Policy>
 static void b5_1_find_first_entry(benchmark::State &state) {
     constexpr auto execution_policy = Policy{};
 
     const auto &size = state.range(0);
 
     // vector with values [0,size)
-    const Container vec1 = suite::generate_increment<Policy, Container>(execution_policy, size, 1);
+    const auto vec1 = suite::generate_increment<Policy>(execution_policy, size, 1);
 
 
     for (auto _: state) {
@@ -45,14 +45,14 @@ static void b5_1_find_first_entry(benchmark::State &state) {
     state.SetBytesProcessed(int64_t(state.iterations()) * actual_size_in_bytes);
 }
 
-template<class Policy, class Container>
+template<class Policy>
 static void b5_1_find_last_entry(benchmark::State &state) {
     constexpr auto execution_policy = Policy{};
 
     const auto &size = state.range(0);
 
     // vector with values [0,size)
-    const auto vec1 = suite::generate_increment<Policy, Container>(execution_policy, size, 1);
+    const auto vec1 = suite::generate_increment<Policy>(execution_policy, size, 1);
 
     for (auto _: state) {
         WRAP_TIMING(auto find_location = B5::b5_1_find(execution_policy, vec1, size - 1);)
@@ -68,14 +68,14 @@ static void b5_1_find_last_entry(benchmark::State &state) {
     state.SetBytesProcessed(int64_t(state.iterations()) * actual_size_in_bytes);
 }
 
-template<class Policy, class Container>
+template<class Policy>
 static void b5_1_find_non_existing_entry(benchmark::State &state) {
     constexpr auto execution_policy = Policy{};
 
     const auto &size = state.range(0);
 
     // vector with values [0,size)
-    const auto vec1 = suite::generate_increment<Policy, Container>(execution_policy, size, 1);
+    const auto vec1 = suite::generate_increment<Policy>(execution_policy, size, 1);
 
     for (auto _: state) {
         WRAP_TIMING(auto find_location = B5::b5_1_find(execution_policy, vec1, -10);)
@@ -91,18 +91,18 @@ static void b5_1_find_non_existing_entry(benchmark::State &state) {
     state.SetBytesProcessed(int64_t(state.iterations()) * actual_size_in_bytes);
 }
 
-#define B5_1_FIND_WRAPPER(datatype) \
+#define B5_1_FIND_WRAPPER \
                                     \
-        BENCHMARK_TEMPLATE2(b5_1_find_first_entry,std::execution::sequenced_policy, datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_first_entry_seq"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(MAX_INPUT_SIZE, MAX_INPUT_SIZE); \
-        BENCHMARK_TEMPLATE2(b5_1_find_first_entry,std::execution::parallel_policy, datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_first_entry_par"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(1 << 2, MAX_INPUT_SIZE);     \
+        BENCHMARK_TEMPLATE1(b5_1_find_first_entry,std::execution::sequenced_policy)->Name(BENCHMARK_NAME("b5_1_find_std::vector<int>_first_entry_seq"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(MAX_INPUT_SIZE, MAX_INPUT_SIZE); \
+        BENCHMARK_TEMPLATE1(b5_1_find_first_entry,std::execution::parallel_policy)->Name(BENCHMARK_NAME("b5_1_find_std::vector<int>_first_entry_par"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(1 << 2, MAX_INPUT_SIZE);     \
                             \
                             \
-        BENCHMARK_TEMPLATE2(b5_1_find_last_entry,std::execution::sequenced_policy,datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_last_entry_seq"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(MAX_INPUT_SIZE, MAX_INPUT_SIZE); \
-        BENCHMARK_TEMPLATE2(b5_1_find_last_entry,std::execution::parallel_policy,datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_last_entry_par"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(1 << 2, MAX_INPUT_SIZE);     \
+        BENCHMARK_TEMPLATE1(b5_1_find_last_entry,std::execution::sequenced_policy)->Name(BENCHMARK_NAME("b5_1_find_std::vector<int>_last_entry_seq"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(MAX_INPUT_SIZE, MAX_INPUT_SIZE); \
+        BENCHMARK_TEMPLATE1(b5_1_find_last_entry,std::execution::parallel_policy)->Name(BENCHMARK_NAME("b5_1_find_std::vector<int>_last_entry_par"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(1 << 2, MAX_INPUT_SIZE);     \
                             \
                             \
-        BENCHMARK_TEMPLATE2(b5_1_find_non_existing_entry,std::execution::sequenced_policy,datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_non_existing_entry_seq"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(MAX_INPUT_SIZE, MAX_INPUT_SIZE); \
-        BENCHMARK_TEMPLATE2(b5_1_find_non_existing_entry,std::execution::parallel_policy,datatype)->Name(BENCHMARK_NAME("b5_1_find_" xstr(datatype) "_non_existing_entry_par"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(1 << 2, MAX_INPUT_SIZE);     \
+        BENCHMARK_TEMPLATE1(b5_1_find_non_existing_entry,std::execution::sequenced_policy)->Name(BENCHMARK_NAME("b5_1_find_std::vector<int>_non_existing_entry_seq"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(MAX_INPUT_SIZE, MAX_INPUT_SIZE); \
+        BENCHMARK_TEMPLATE1(b5_1_find_non_existing_entry,std::execution::parallel_policy)->Name(BENCHMARK_NAME("b5_1_find_std::vector<int>_non_existing_entry_par"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(1 << 2, MAX_INPUT_SIZE);     \
 
 //endregion b5_1_find
 
@@ -115,7 +115,7 @@ static void b5_2_partition_wrapper(benchmark::State &state) {
     const auto &size = state.range(0);
 
     // vector with values [0,size)
-    auto vec1 = suite::generate_increment(execution_policy, size, 1);
+    auto vec1 = suite::generate_increment<Policy>(execution_policy, size, 1);
 
     for (auto _: state) {
         WRAP_TIMING(auto find_location = B5::b5_2_partition(execution_policy, vec1);)
@@ -142,10 +142,10 @@ static void b5_3_unique_copy_default_wrapper(benchmark::State &state) {
     const auto &size = state.range(0);
 
     // vector with value 1 of length size
-    suite::int_vec vec1(size);
+    auto vec1 = suite::get_vec<Policy>(size);
     suite::fill_init<Policy>(vec1, 1);
 
-    suite::int_vec result(1);
+    auto result = suite::get_vec<Policy>(1);
     suite::fill_init<Policy>(result, 0);
 
     for (auto _: state) {
@@ -173,7 +173,7 @@ static void b5_4_minmax_element_all_equal(benchmark::State &state) {
     const auto &size = state.range(0);
 
     // vector with value 1
-    auto vec1 = suite::generate_increment(execution_policy, size, 1, 0);
+    auto vec1 = suite::generate_increment<Policy>(execution_policy, size, 1, 0);
 
     for (auto _: state) {
         WRAP_TIMING(const auto res = B5::b5_4_minmax_element(execution_policy, vec1);)
@@ -219,8 +219,7 @@ static void b5_4_minmax_element_increasing(benchmark::State &state) {
 // Register the function as a benchmark
 #define B5_GROUP_BENCHMARKS \
                             \
-        B5_1_FIND_WRAPPER(std::vector<int>) \
-        B5_1_FIND_WRAPPER(std::deque<int>)  \
+        B5_1_FIND_WRAPPER   \
                             \
                             \
         BENCHMARK_TEMPLATE1(b5_2_partition_wrapper,std::execution::sequenced_policy)->Name(BENCHMARK_NAME("b5_2_partition_seq"))->CUSTOM_STATISTICS->RangeMultiplier(2)->Range(MAX_INPUT_SIZE, MAX_INPUT_SIZE); \
