b3771c33e2112791969f964af5d59db11397ae47
Ruben Laso <laso@par.tuwien.ac.at>
2023-10-06 11:09:08 +0200

    Change to if constexpr when considering parallel policies

--- src/benchmarks/index_based_iterations/b9_7_transform_omp.h
+++ src/benchmarks/index_based_iterations/b9_7_transform_omp.h
@@ -8,21 +8,21 @@ namespace B9 {
 
 	const auto b9_7_transform_omp = [] (auto && policy, const auto & input_data, auto & res, auto && f) {
 		// if policy is std::execution::parallel_unsequenced_policy -> parallelization + vectorization
-		if (std::is_same_v<decltype(policy), std::execution::parallel_unsequenced_policy>) {
-#pragma omp parallel for simd
+		if constexpr (std::is_same_v<decltype(policy), std::execution::parallel_unsequenced_policy>) {
+#pragma omp parallel for simd default(none) shared(input_data, res, f)
 			for (std::size_t i = 0; i < input_data.size(); ++i) {
 				res[i] = f(input_data[i]);
 			}
 		}
 			// if policy is std::execution::parallel_policy -> parallelization
-		else if (std::is_same_v<decltype(policy), std::execution::parallel_policy>) {
-#pragma omp parallel for
+		else if constexpr (std::is_same_v<decltype(policy), std::execution::parallel_policy>) {
+#pragma omp parallel for default(none) shared(input_data, res, f)
 			for (std::size_t i = 0; i < input_data.size(); ++i) {
 				res[i] = f(input_data[i]);
 			}
 		}
 			// if policy is std::execution::unsequenced_policy -> vectorization
-		else if (std::is_same_v<decltype(policy), std::execution::unsequenced_policy>) {
+		else if constexpr (std::is_same_v<decltype(policy), std::execution::unsequenced_policy>) {
 #pragma omp simd
 			for (std::size_t i = 0; i < input_data.size(); ++i) {
 				res[i] = f(input_data[i]);
