c14bd395fb160fe403b0c77e22d05d56a3f71ade
Ruben Laso <laso@par.tuwien.ac.at>
2023-10-18 13:52:24 +0200

    Workaround for bug (memory leak) in parallel std::sort

--- src/benchmarks/sort/sort_std.h
+++ src/benchmarks/sort/sort_std.h
@@ -4,10 +4,20 @@
 #include <algorithm>
 #include <execution>
 
+#ifdef USE_TBB
+#include <tbb/parallel_sort.h>
+#endif //USE_TBB
+
 namespace benchmark_sort
 {
 	const auto sort_std = [](auto && executionPolicy, auto & input_data) {
+#ifdef USE_TBB
+		// Show compiler message if TBB is used
+#warning "Using TBB parallel_sort"
+		tbb::parallel_sort(input_data.begin(), input_data.end());
+#else
 		std::sort(executionPolicy, input_data.begin(), input_data.end());
+#endif //USE_TBB
 	};
 }
 
