65c8d26095a73bb258d93e0545e1dd2374f27b82
Ruben Laso <laso@par.tuwien.ac.at>
2023-12-01 16:00:59 +0100

    Move list of benchmarks to a header of its own

---
+++ src/benchmarks/pstl-benchmarks.h
@@ -0,0 +1,70 @@
+#ifndef PSTL_BENCH_PSTL_BENCHMARKS_H
+#define PSTL_BENCH_PSTL_BENCHMARKS_H
+
+#include "adjacent_difference/group.h"
+#include "adjacent_find/group.h"
+#include "all_of/group.h"
+#include "any_of/group.h"
+#include "copy/group.h"
+#include "copy_if/group.h"
+#include "copy_n/group.h"
+#include "count/group.h"
+#include "count_if/group.h"
+#include "equal/group.h"
+#include "exclusive_scan/group.h"
+#include "fill/group.h"
+#include "find/group.h"
+#include "for_each/group.h"
+#include "generate/group.h"
+#include "includes/group.h"
+#include "inclusive_scan/group.h"
+#include "inplace_merge/group.h"
+#include "is_sorted/group.h"
+#include "lexicographical_compare/group.h"
+#include "max_element/group.h"
+#include "merge/group.h"
+#include "min_element/group.h"
+#include "mismatch/group.h"
+#include "none_of/group.h"
+#include "partial_sort/group.h"
+#include "partition/group.h"
+#include "reduce/group.h"
+#include "search/group.h"
+#include "sort/group.h"
+#include "transform/group.h"
+#include "transform_reduce/group.h"
+
+ADJACENT_DIFFERENCE_GROUP
+ADJACENT_FIND_GROUP
+ANY_OF_GROUP
+ALL_OF_GROUP
+COPY_GROUP
+COPY_IF_GROUP
+COPY_N_GROUP
+COUNT_GROUP
+COUNT_IF_GROUP
+EQUAL_GROUP
+EXCLUSIVE_SCAN_GROUP
+FILL_GROUP
+FIND_GROUP
+FOR_EACH_GROUP
+GENERATE_GROUP
+INCLUDES_GROUP
+INPLACE_MERGE_GROUP
+INCLUSIVE_SCAN_GROUP
+IS_SORTED_GROUP
+LEXICOGRAPHICAL_COMPARE_GROUP
+MAX_ELEMENT_GROUP
+MERGE_GROUP
+MIN_ELEMENT_GROUP
+MISMATCH_GROUP
+NONE_OF_GROUP
+PARTIAL_SORT_GROUP
+PARTITION_GROUP
+REDUCE_GROUP
+SEARCH_GROUP
+SORT_GROUP
+TRANSFORM_GROUP
+TRANSFORM_REDUCE_GROUP
+
+#endif //PSTL_BENCH_PSTL_BENCHMARKS_H
