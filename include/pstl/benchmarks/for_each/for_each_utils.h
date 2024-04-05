89367402eb95bfe12359815cf8efd44551f8c4bc
Ruben Laso Rodriguez <laso@par.tuwien.ac.at>
2024-04-05 16:48:36 +0200

    Number of iterations as env. variable in for_each

--- include/pstl/benchmarks/for_each/for_each_utils.h
+++ include/pstl/benchmarks/for_each/for_each_utils.h
@@ -9,8 +9,12 @@
 
 namespace benchmark_for_each
 {
-	const auto kernel = [](auto & input) {
-		volatile size_t I = 1;
+#ifndef DEFAULT_FOR_EACH_KERNEL_ITS
+#define DEFAULT_FOR_EACH_KERNEL_ITS 1
+#endif
+
+	const auto kernel = [](auto & input, const auto Its) {
+		volatile size_t I = Its;
 
 		pstl::elem_t a{};
 
@@ -31,12 +35,20 @@ namespace benchmark_for_each
 
 		auto data = pstl::generate_increment(execution_policy, size);
 
+		// Read environment variable PSTL_FOR_EACH_KERNEL_ITS
+		std::size_t its = DEFAULT_FOR_EACH_KERNEL_ITS;
+
+		const char * env_p = std::getenv("PSTL_FOR_EACH_KERNEL_ITS");
+		if (env_p) { its = std::strtoul(env_p, nullptr, 10); }
+
 		for (auto _ : state)
 		{
-			WRAP_TIMING(f(execution_policy, data, kernel);)
+			WRAP_TIMING(f(execution_policy, data, [=](auto & elem) { kernel(elem, its); }));
 		}
 
 		state.SetBytesProcessed(pstl::computed_bytes(state, data));
+
+		state.counters["kernel_its"] = static_cast<double>(its);
 	}
 } // namespace benchmark_for_each
 
